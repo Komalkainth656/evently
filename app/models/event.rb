d6707e636d1267a2f1e994d6bb1241dcdd5e80ca
komal <kainthkomal656@gmail.com>
2020-08-04 11:50:46 -0700

    validations added for price and title

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,11 @@
 class Event < ApplicationRecord
+    before_validation : default_price
+    DEFAULT_PRICE = 0 
+    def default_price
+        self.price ||= DEFAULT_PRICE
+      end
+    validates(:title,
+        presence: true,
+        uniqueness: true,
+      )
 end
